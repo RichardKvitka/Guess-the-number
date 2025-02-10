acf051672993ff1bdbac9c9d6e306e629c554eeb
Oleksandr Tsakhlo <richardkvitka55@gmail.com>
2025-02-10 12:29:33 +0100

    Create art.py

---
+++ art.py
@@ -0,0 +1,13 @@
+logo = """
+
+  ,ad8888ba,                                                                          
+ d8"'    `"8b                                                                         
+d8'                                                                                   
+88             88       88   ,adPPYba,  ,adPPYba,  ,adPPYba,   ,adPPYba,  8b,dPPYba,  
+88      88888  88       88  a8P_____88  I8[    ""  I8[    ""  a8P_____88  88P'   "Y8  
+Y8,        88  88       88  8PP"""""""   `"Y8ba,    `"Y8ba,   8PP"""""""  88          
+ Y8a.    .a88  "8a,   ,a88  "8b,   ,aa  aa    ]8I  aa    ]8I  "8b,   ,aa  88          
+  `"Y88888P"    `"YbbdP'Y8   `"Ybbd8"'  `"YbbdP"'  `"YbbdP"'   `"Ybbd8"'  88          
+
+
+"""
